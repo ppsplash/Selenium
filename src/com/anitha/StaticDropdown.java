aef3a500786dab3b5019ce33e8f6c90233d55f6a
Anitha <ppsplash13@gmail.com>
2023-01-18 19:48:28 +0100

    Added website url

---
+++ src/com/anitha/StaticDropdown.java
@@ -0,0 +1,14 @@
+package com.anitha;
+
+import org.openqa.selenium.WebDriver;
+
+public class StaticDropdown {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://google.com");
+
+	}
+
+}
