27b87ecd3863081cc3c9fa6abadbee1f95a48465
Anitha <ppsplash13@gmail.com>
2023-01-21 20:53:50 +0100

    Dynamic dropdown

---
+++ src/com/anitha/updatedDropdown.java
@@ -0,0 +1,25 @@
+package com.anitha;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class updatedDropdown {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		driver.findElement(By.id("divpaxinfo")).click();
+		Thread.sleep(2000);
+		int i = 1;
+		while(i<5)
+		{
+		driver.findElement(By.id("hrefIncAdt")).click();
+		i++;
+		}
+		System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+	}
+
+}
