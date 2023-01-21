8a527fc4d4cc767f8369519f9a920e854cb0e5ca
Anitha <ppsplash13@gmail.com>
2023-01-21 20:59:27 +0100

    for loop

--- src/com/anitha/updatedDropdown.java
+++ src/com/anitha/updatedDropdown.java
@@ -13,11 +13,16 @@ public class updatedDropdown {
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
 		driver.findElement(By.id("divpaxinfo")).click();
 		Thread.sleep(2000);
-		int i = 1;
+		/*int i = 1;
 		while(i<5)
 		{
 		driver.findElement(By.id("hrefIncAdt")).click();
 		i++;
+		}*/
+		System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+		for(int i=1;i<5;i++)
+		{
+			driver.findElement(By.id("hrefIncAdt")).click();	
 		}
 		System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
 	}
