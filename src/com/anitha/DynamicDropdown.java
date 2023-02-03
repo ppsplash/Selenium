fbb5f3f0e2aa7da9eb093e1e59a0e201c788f5d4
Anitha <ppsplash13@gmail.com>
2023-02-03 15:20:08 +0100

    window max to run all automated script

--- src/com/anitha/DynamicDropdown.java
+++ src/com/anitha/DynamicDropdown.java
@@ -10,11 +10,12 @@ import org.testng.Assert;
 public class DynamicDropdown {
 
 public static void main(String[] args) throws InterruptedException {
-
 WebDriver driver =new ChromeDriver();
 
 driver.get("https://rahulshettyacademy.com/dropdownsPractise/"); 
 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+driver.manage().window().maximize();
+
 
 driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
 
@@ -30,6 +31,28 @@ driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStat
 
 driver.findElement(By.cssSelector(".ui-state-default.ui-state-highlight")).click();
 driver.quit();
+	
+/*WebDriver driver =new ChromeDriver();
+
+driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+
+Thread.sleep(7000);
+
+// OriginStation
+
+ driver.findElement(By.xpath(".//*[@id='ctl00_mainContent_ddl_originStation1_CTXT']")).click();
+
+driver.findElement(By.cssSelector("a[value='DEL']")).click();     
+
+	// Destination
+
+driver.findElement(By.xpath(".//*[@id='ctl00_mainContent_ddl_destinationStation1_CTXT']")).click();
+
+driver.findElement(By.xpath("(//a[@value='HYD'])[2]")).click();
+
+Thread.sleep(5000);
+
+driver.findElement(By.cssSelector(".ui-state-default.ui-state-highlight")).click();*/
 
 }
 
