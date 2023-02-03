6df1e946256eb4b4ea8d809932f407e9f74027ba
Anitha <ppsplash13@gmail.com>
2023-02-03 15:18:23 +0100

    alert method

---
+++ src/com/anitha/Assignment.java
@@ -0,0 +1,26 @@
+package com.anitha;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Assignment {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		String text = "Anitha";
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		driver.findElement(By.id("name")).sendKeys(text);
+		driver.findElement(By.cssSelector("[id='alertbtn']")).click();
+		System.out.println(driver.switchTo().alert().getText());
+		Thread.sleep(2000);
+		driver.switchTo().alert().accept();
+		driver.findElement(By.xpath("//input[@id='confirmbtn']")).click();
+		System.out.println(driver.switchTo().alert().getText());
+		driver.switchTo().alert().dismiss();
+
+	}
+
+}
