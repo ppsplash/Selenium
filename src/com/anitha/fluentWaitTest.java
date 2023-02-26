918fa88c2bd1fd3914300d5ff0880dcb4849c828
Anitha <ppsplash13@gmail.com>
2023-02-26 20:31:55 +0100

    fluentwait

---
+++ src/com/anitha/fluentWaitTest.java
@@ -0,0 +1,17 @@
+package com.anitha;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class fluentWaitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
+		driver.findElement(By.cssSelector("div[id='start'] button")).click();
+		
+	}
+
+}
