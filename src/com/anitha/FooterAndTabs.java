0b7dd24c37c9ea58ff77b70ce183830ec5949717
Anitha <ppsplash13@gmail.com>
2023-03-16 15:09:28 +0100

    Clicking on link using keys and iterating the window handles to get title of each page

--- src/com/anitha/FooterAndTabs.java
+++ src/com/anitha/FooterAndTabs.java
@@ -1,9 +1,12 @@
 package com.anitha;
 
 import java.time.Duration;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -21,6 +24,20 @@ public class FooterAndTabs {
 		System.out.println(footerdriver.findElements(By.tagName("a")).size());
 		WebElement columndriver = footerdriver.findElement(By.xpath("//div/table/tbody/tr/td[1]/ul"));
 		System.out.println(columndriver.findElements(By.tagName("a")).size());
+		for(int i =0;i<columndriver.findElements(By.tagName("a")).size();i++)
+		{
+			String clickOnLink = Keys.chord(Keys.CONTROL,Keys.ENTER);
+			columndriver.findElements(By.tagName("a")).get(i).sendKeys(clickOnLink);
+			
+		}
+		Set<String> a= driver.getWindowHandles();
+		Iterator<String> it = a.iterator();
+		while(it.hasNext())
+		{
+			driver.switchTo().window(it.next());
+			System.out.println(driver.getCurrentUrl());
+		}
+		driver.quit();
 		
 	}
 
